15bfd436beeee80f485a1a3190528ac77880ae4f
Aditya Kudalkar <adityakudalkar914@gmail.com>
2025-12-20 21:42:50 +0530

    Initial commit

---
+++ app/src/main/java/com/techtitans/prepsprint_project/AI/AiChatBot.kt
@@ -0,0 +1,22 @@
+package com.techtitans.prepsprint_project.AI
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.techtitans.prepsprint_project.R
+
+class AiChatBot : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ai_chat_bot2, container, false)
+    }
+
+
+}
